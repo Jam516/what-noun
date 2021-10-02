231c191b84ab33c88b2413f354c3852344983af2
Jam516 <kufuorkofi@gmail.com>
2021-10-03 00:28:52 +0100

    change api

--- api/index.ts
+++ api/index.ts
@@ -2,41 +2,37 @@ import axios from "axios";
 //INSERT YOUR BASE ID AND TABLE
 //EXAMPLE https://api.airtable.com/v0/BASE_ID/table/
 axios.defaults.baseURL =
-  "https://api.airtable.com/v0/appuFWXmw0frhaUcW/avatars/";
+  "https://api.steinhq.com/v1/storages/6158d04447873c2b735e69b4/";
 //INSERT YOUR KEY API
-axios.defaults.headers["Authorization"] = "Bearer keye0U1hC2uyCZeR4";
-axios.defaults.headers.post["Content-Type"] = "application/json";
+// axios.defaults.headers["Authorization"] = "Bearer keye0U1hl/json";
 export async function getNoun(form?: string) {
   //param form is equal var Form on Hero component
 
   try {
-    //MAX data = 5
+    var getrequest = "/Sheet1?search={\"wallet\":\""+ form +"\"}"
+
     const data = await axios
-      .get("/?maxRecords=40")
-      .then((res) => res.data.records);
+      .get(getrequest);
     // setting data entry
-    let result;
-    for (const res of data) {
-      if (res.fields.wallet === form) {
-        result = {
-          avatar: res.fields.avatar,
-          description: res.fields.description,
-          title: res.fields.title,
-          // url: res.fields.status[0].url,
-          url: res.fields.status,
-        };
-      }
-    }
-    if (result === undefined) {
-      result = result = {
-        avatar: data[0].fields.avatar,
-        description: data[0].fields.description,
-        title: data[0].fields.title,
-        // url: data[0].fields.status[0].url,
-        url: res.fields.status[0],
+    // let result;
+    if (data.wallet === form) {
+      result = {
+        description: data.description,
+        title: data.title,
+        // url: res.fields.status[0].url,
+        url: data.status,
       };
     }
-    return result;
+
+    // if (result === undefined) {
+    //   result = result = {
+    //     description: data[0].description,
+    //     title: data[0].title,
+    //     // url: data[0].fields.status[0].url,
+    //     url: res.status[0],
+    //   };
+    // }
+    // return result;
   } catch (err) {
     console.log(err);
   }
